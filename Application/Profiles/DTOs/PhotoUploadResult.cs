a3ed594d021d01dee595be437e04b126d262665e
drewvandev <drewvandev@gmail.com>
2026-03-03 06:06:25 -0500

    End of Section 16

---
+++ Application/Profiles/DTOs/PhotoUploadResult.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Application.Profiles.DTOs;
+
+public class PhotoUploadResult
+{
+    public required string PublicId { get; set; }
+    public required string Url { get; set; }
+}
+
+
